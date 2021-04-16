5df87a062d7535d70d9b3a5368f6b8e4a8819cd5
heejin-hwang <heyday.xz@gmail.com>
2021-04-16 18:03:21 +0900

    Init

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+function About() {
+    return <div>오 넥스트</div>
+}
+
+export default About
---
+++ pages/user/[id].jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useRouter} from "next/router";
+
+const User = () => {
+    const router = useRouter();
+
+    const {id} = router.query;
+
+    return <div>{id} 번 user 의 페이지 입니다.</div>
+}
+
+export default User;
